f6e3c8927ccb81f4b9c63b690901a40679c8fb3c
Luhojuo <juholuoma2@gmail.com>
2018-05-14 19:58:43 +0300

    Update test.py

--- Dimensionality Reduction/test.py
+++ Dimensionality Reduction/test.py
@@ -9,57 +9,77 @@ from mpl_toolkits.mplot3d import Axes3D
 #import seaborn as sns
 import pandas as pd
 import numpy as np
+import argparse
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
+ap = argparse.ArgumentParser()
 
-df = pd.read_csv('C:/Users/Omistaja/Documents/VaihdonKurssit/MachineLearningAndStuff/Results/FeaturesXception.csv', header='infer')
-#print(df['class'])
+ap.add_argument("-data", "--data", required=True,
+	help="dataset to be used")
+
+ap.add_argument("-class", "--class", required = False,
+	help="The class variable")
+
+args = vars(ap.parse_args())
 
 
-for i in range(len(df['class'])):
-	if (df['class'][i] == "cat"):
-		df.loc[i,'class'] = 1
 
-	elif(df['class'][i] == "clock"):
-		df.loc[i,'class'] = 2
 
-	elif(df['class'][i] == "dog"):
-		df.loc[i,'class'] = 3
+classVar = pd.read_csv(args['class'], header='infer')
 
-	elif(df['class'][i] == "giraffe"):
-		df.loc[i,'class'] = 4
+features = pd.read_csv(args['data'], header=None)
+#print(features.head(n=5))
+df = pd.concat([classVar['x'].reset_index(), features], axis=1)
 
-	elif(df['class'][i] == "pizza"):
-		df.loc[i,'class'] = 5
 
-	elif(df['class'][i] == "skis"):
-		df.loc[i,'class'] = 6
 
-	elif(df['class'][i] == "surfboard"):
-		df.loc[i,'class'] = 7
+for i in range(len(df['x'])):
+	if (df['x'][i] == "cat"):
+		df.loc[i,'x'] = 1
 
-	elif(df['class'][i] == "tennis racket"):
-		df.loc[i,'class'] = 8
+	elif(df['x'][i] == "clock"):
+		df.loc[i,'x'] = 2
 
-	elif(df['class'][i] == "toilet"):
-		df.loc[i,'class'] = 9
+	elif(df['x'][i] == "dog"):
+		df.loc[i,'x'] = 3
 
-	elif(df['class'][i] == "traffic light"):
-		df.loc[i,'class'] = 10	
+	elif(df['x'][i] == "giraffe"):
+		df.loc[i,'x'] = 4
+
+	elif(df['x'][i] == "pizza"):
+		df.loc[i,'x'] = 5
+
+	elif(df['x'][i] == "skis"):
+		df.loc[i,'x'] = 6
+
+	elif(df['x'][i] == "surfboard"):
+		df.loc[i,'x'] = 7
+
+	elif(df['x'][i] == "tennis racket"):
+		df.loc[i,'x'] = 8
+
+	elif(df['x'][i] == "toilet"):
+		df.loc[i,'x'] = 9
+
+	elif(df['x'][i] == "traffic light"):
+		df.loc[i,'x'] = 10	
 	
 
 
-#print(df['class'])			
+del df['index']
 
 
 from sklearn.model_selection import train_test_split
 # test_size: what proportion of original data is used for test set
-train_img, test_img, train_lbl, test_lbl = train_test_split( df, df['class'], test_size=1/7.0, random_state=0)
+train_img, test_img, train_lbl, test_lbl = train_test_split(df, df['x'], test_size=1/7.0, random_state=0)
+
+#print(df.head(n=5))
 
 # Fit on training set only.
 scaler.fit(train_img)
+
 # Apply transform to both the training set and the test set.
 train_img = scaler.transform(train_img)
 test_img = scaler.transform(test_img)
@@ -86,3 +106,5 @@ print(logisticRegr.predict(test_img[0].reshape(1,-1)))
 print(logisticRegr.predict(test_img[0:10]))
 
 print(logisticRegr.score(test_img, test_lbl))
+
+
